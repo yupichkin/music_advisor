02d7655a9e64690bb50ff1a50ad42125a98a4e47
yupichkin <darizhapoff2016@gmail.com>
2021-11-19 18:05:46 +0300

    test commit

--- Music Advisor/task/src/advisor/controller/RequestMethod.java
+++
@@ -1,9 +0,0 @@
-package advisor.controller;
-
-import java.io.IOException;
-
-public interface RequestMethod {
-    void handleCommand(final String accessToken,
-                       final String apiPath,
-                       final String addOnArgument); //third argument for playlists
-}
